3568b77c5a10ebd11acd76e30cb03394ac2d3e43
Bakhriev <legendofpain5112@gmail.com>
2023-10-25 22:25:57 +0500

    last

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,17 @@
 import {burgerMenu} from './functions/burger'
 
 burgerMenu()
+
+const dropdowns = document.querySelectorAll('.menu-item-has-children')
+
+dropdowns.forEach(dropdown => {
+	const subMenu = dropdown.querySelector('.sub-menu')
+	dropdown.addEventListener('click', toggleSubMenu)
+})
+
+function toggleSubMenu() {
+	const subMenu = this.querySelector('.sub-menu')
+	subMenu.style.maxHeight = subMenu.style.maxHeight
+		? ''
+		: subMenu.scrollHeight + 'px'
+}
