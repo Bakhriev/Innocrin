13cbdd9e607f87c84a5785a3eedb3a51955405df
Bakhriev <legendofpain5112@gmail.com>
2023-10-25 22:29:31 +0500

    last

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,31 @@
-import {burgerMenu} from './functions/burger'
+export const burgerMenu = () => {
+	const burger = document.querySelector('.burger')
+	const mobileMenu = document.querySelector('.mobile-menu')
+	const overlay = document.querySelector('.overlay')
+	const body = document.querySelector('body')
 
+	const toggleActiveClass = () => {
+		burger.classList.toggle('active')
+		overlay.classList.toggle('active')
+		mobileMenu.classList.toggle('active')
+		body.classList.toggle('disable-scroll')
+	}
+
+	burger.addEventListener('click', toggleActiveClass)
+
+	overlay.addEventListener('click', toggleActiveClass)
+
+	window.addEventListener('resize', () => {
+		const {innerWidth} = window
+		if (innerWidth > 991.98) {
+			burger.classList.remove('active')
+			overlay.classList.remove('active')
+			mobileMenu.classList.remove('active')
+			body.classList.remove('disable-scroll')
+		}
+	})
+}
 burgerMenu()
-
 const dropdowns = document.querySelectorAll('.menu-item-has-children')
 
 dropdowns.forEach(dropdown => {
